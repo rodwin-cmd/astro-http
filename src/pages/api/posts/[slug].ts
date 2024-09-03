3fcc98efed53d4105bc77d06bbe5228bd966612d
rodwin osorio <rodwinosorio@gmail.com>
2024-09-02 22:56:38 -0500

    ok

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -3,26 +3,26 @@ import { getEntry } from "astro:content";
 
 export const prerender = false;
 
-export const GET:APIRoute = async({params, request})=> {
-  
-    
-    const {slug} = params;
+export const GET: APIRoute = async ({ params, request }) => {
+  const { slug } = params;
 
-    const post = await getEntry('blog', slug as any);
+  const post = await getEntry("blog", slug as any);
 
-    if (!post){
-        return new Response(JSON.stringify({msg:`Post ${slug} not found`}),{
-            status: 404,
-            headers:{
-                'Content-Type': 'application/json',
-            }
-        })
-    }
+  if (!post) {
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    return new Response(
-        JSON.stringify(post),
-        {status:200}
-    );
+  return new Response(JSON.stringify(post), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 // export const getStaticPaths:GetStaticPaths = async() =>{
--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,16 +1,38 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({params, request}) => {
+export const GET: APIRoute = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
 
-   const posts = await getCollection('blog');
+  if (slug) {
+    const post = await getEntry("blog", slug);
 
-    return new Response(JSON.stringify(posts), {
-        status : 200,
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
         headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-}
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  }
+
+  return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+    status: 404,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const posts = await getCollection("blog");
+
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
