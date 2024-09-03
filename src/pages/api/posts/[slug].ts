8dd95bb4c42c1fd6dbb6b4c9805678de271e363b
rodwin osorio <rodwinosorio@gmail.com>
2024-09-02 20:33:23 -0500

    agregando server actions

---
+++ src/pages/api/posts/[slug].ts
@@ -0,0 +1,34 @@
+import type { APIRoute, GetStaticPaths } from "astro";
+import { getEntry } from "astro:content";
+
+export const prerender = false;
+
+export const GET:APIRoute = async({params, request})=> {
+  
+    
+    const {slug} = params;
+
+    const post = await getEntry('blog', slug as any);
+
+    if (!post){
+        return new Response(JSON.stringify({msg:`Post ${slug} not found`}),{
+            status: 404,
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+    }
+
+    return new Response(
+        JSON.stringify(post),
+        {status:200}
+    );
+};
+
+// export const getStaticPaths:GetStaticPaths = async() =>{
+//     return[
+//         {
+//             params: {slug: 'First-post'},
+//         }
+//     ]
+// }
