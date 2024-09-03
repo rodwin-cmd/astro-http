a4293806a031f860b8c6918fdb2547d4e506ca65
rodwin osorio <rodwinosorio@gmail.com>
2024-09-02 23:04:49 -0500

    add cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: cloudflare(),
 });
